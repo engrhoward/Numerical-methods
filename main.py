80b48f729d9f5edb13c4ecc03158f759462a8992
engrhoward <142307055+engrhoward@users.noreply.github.com>
2023-09-12 20:20:02 +0800

    9/12

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title("Numerical Methods")
 
@@ -21,7 +22,14 @@ with integ:
     
     with tab_a:
         
+        #input function
         function = st.text_input("Function")
+
+        # create a function that converts ^ to **
+        def convert_syntax(input_str):
+            output_str = input_str.replace('^','**')
+            return output_str
+        
         
         col1, col2 = st.columns(2) # create column for upper and lower inpt
         with col1:
@@ -36,7 +44,7 @@ with integ:
         delta_x = (float(upper_limit) - float(lower_limit))/bins
         
         def f(x):
-            return eval(function)
+            return eval(convert_syntax(function))
         
         #latex of function
         if function and lower_limit is not None and upper_limit is not None:
@@ -49,17 +57,30 @@ with integ:
         def rectangular_integration(func,lower_limit,upper_limit,bins):
             integral = 0.0
 
+            x_midpoints = []
             for i in range(bins):
                 x_midpoint = lower_limit + (i+0.5)*delta_x
                 integral = integral + func(x_midpoint)*delta_x
+
+                x_midpoints.append(x_midpoint)
                 
-            return integral
-        
+            return integral, x_midpoints
+        integral, x_midpoints = rectangular_integration(f, lower_limit, upper_limit, bins)
+
         if function and lower_limit is not None and upper_limit is not None:
-            result = rectangular_integration(f, lower_limit, upper_limit, bins)
+            result = integral
             st.write("Approximate integral:", result)
 
         
-        
+        if function and lower_limit is not None and upper_limit is not None:
+            x_values = np.linspace(lower_limit, upper_limit, 100)
+            y_values = [f(x) for x in x_values]
 
-
+            plt.figure(figsize=(10, 6))
+            plt.plot(x_values, y_values, label=f'Function: {function}')
+            plt.fill_between(x_midpoints, 0, [f(x) for x in x_midpoints], alpha=0.2, label='Rectangular Area')
+            plt.xlabel('x')
+            plt.ylabel('f(x)')
+            plt.title('Function and Rectangular Approximation')
+            plt.legend()
+            st.pyplot(plt)
