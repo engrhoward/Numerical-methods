2c99249dbc90a6f1e3760ca8ab005fa69dc4e5bc
engrhoward <142307055+engrhoward@users.noreply.github.com>
2023-09-23 18:03:54 +0800

    9/23

--- main.py
+++ main.py
@@ -102,8 +102,8 @@ with integ:
 
             plt.figure(figsize=(10, 6))
             plt.plot(x_values, y_values, label=f'Function: {function}')
-            plt.bar(x_midpoints, [f(x) for x in x_midpoints], width=delta_x, alpha=0.5, 
-                    label='Rectangular Approximation', edgecolor = 'black',
+            plt.bar(x_midpoints, [f(x) for x in x_midpoints], width=delta_x, alpha=0.5,
+                    color = 'blue', label='Rectangular Approximation', edgecolor = 'black',
                     linewidth = 2)
             plt.scatter(x_midpoints, [f(x) for x in x_midpoints], color = 'black')
             plt.xlabel('x')
@@ -113,6 +113,22 @@ with integ:
             st.pyplot(plt)
 
     with tab_b:
+        st.markdown(
+            """
+            <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px;">
+                <p style="font-size: 18px; font-weight: bold; color: #333;">Trapezoidal Summation (Midpoint)</p>
+                <p style="font-size: 16px; line-height: 1.4; color: #555;">
+                    Trapezoidal Summation uses, well, trapezoid to estimate the area under the function.
+                </p>
+                <p style="font-size: 16px; line-height: 1.4; color: #555;">
+                    The summation form of this technique is expressed as:
+                </p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+        st.latex(r'\int_a^b f(x) \, dx \approx \frac{\Delta x}{2} \left[ f(a) + 2\sum_{i=1}^{n-1} f(x_i) + f(b) \right]')
+
         function_2 = st.text_input('Function', key='Trapezoidal')
 
         trap_upper, trap_lower = st.columns(2)
@@ -158,9 +174,18 @@ with integ:
             integral *= delta_x
 
             # Plot the function curve
-            x_values = np.linspace(lower_limit, upper_limit, 100)
-            y_values = [func(x) for x in x_values]
-            plt.plot(x_values, y_values, label=f'Function: {function_2}')
+            x_values_exact = np.linspace(lower_limit, upper_limit, 100)
+            y_values_exact = [func(x) for x in x_values_exact]
+            plt.plot(x_values_exact, y_values_exact, label=f'Function: {function_2}',
+                     linewidth = 1, color = 'black')
+            plt.scatter(x_values,y_values)
+
+            for i in range(len(x_values) - 1):
+                x_segment = [x_values[i], x_values[i + 1]]
+                y_segment = [y_values[i], y_values[i + 1]]
+                plt.plot(x_segment, y_segment, color='red', linewidth = 1)
+                plt.fill_between(x_segment, y_segment, color = 'blue',
+                                 alpha = 0.5)
 
             # Customize the plot
             plt.xlabel('x')
--- numerical_integration.py
+++ numerical_integration.py
@@ -1,8 +1,53 @@
-def trapezoidal_integration(func,lower_limit,upper_limit,bins):
+import matplotlib.pyplot as plt
+
+x_values = [1,2,3,4,5]
+y_values = [2,4,9,16,25]
+
+x_values_segments = []
+y_values_segments = []
+
+for i in range(len(x_values)):
+    x_values_segment = [x_values[i], x_values[i+1]]
+    y_values_segment = [y_values[i], y_values[i+1]]
+
+    plt.plot(x_values_segment, y_values_segment, 
+             color = 'red', linestyle = "--")
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def rectangular_integration(func, lower_limit, upper_limit, bins):
     integral = 0.0
+
+    x_midpoints = []
     
-    x_values = np.linspace(lower_limit, upper_limit, bins +1)
-    y_values = [func]
+    # Define a default behavior when no function is provided
+    def default_function(x):
+        return 0.0
 
+    if func:  # Check if a function is provided
+        for i in range(bins):
+            x_midpoint = lower_limit + (i + 0.5) * delta_x
+            integral = integral + func(x_midpoint) * delta_x
+            x_midpoints.append(x_midpoint)
+    else:
+        # Use the default function if no function is provided
+        integral = default_function(lower_limit) * (upper_limit - lower_limit)
 
-            
+    return integral, x_midpoints
